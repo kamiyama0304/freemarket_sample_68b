ee81c1fcc2adc53fb5a6f9ee4418203be4eef7b4
shin usuda <sasasa.24.blue@gmail.com>
2020-02-19 17:33:09 +0900

    add destroy function without challenge

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,13 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def destroy
+    product = Product.find(parmas[:id])
+    if product.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
 end
