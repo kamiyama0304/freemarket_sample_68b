13ad31b0380a157ad78146af382f2ff93db47154
KI-RA-CO <kihara4173@icloud.com>
2020-02-21 17:02:34 +0900

    change function　blandCategory to productList
    トップページのブランド別の表示機能を廃止し、商品を一覧で表示する機能に変更した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @newProducts = Product.includes(:images).where(status: 0).order("id DESC")
+    @newProducts = Product.includes(:images).where(status: 0).order("price ASC")
     @brandProducts = Product.includes(:images).where(status: 0).order("id DESC").limit(3)
   end
 end
