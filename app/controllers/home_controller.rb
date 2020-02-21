3f250b54e51f201794e764278d4d38d2da60657c
KI-RA-CO <kihara4173@icloud.com>
2020-02-21 19:26:48 +0900

    change function product list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @newProducts = Product.includes(:images).where(status: 0).order("price ASC")
-    @brandProducts = Product.includes(:images).where(status: 0).order("id DESC").limit(3)
+    @newProducts = Product.includes(:images).where(status: 0).order("RAND()")
+    @brandProducts = Product.includes(:images).where(status: 0).order("id DESC")
   end
 end
 
