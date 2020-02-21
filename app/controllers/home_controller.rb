61053a80eb516b951e905d383daa77f870d23c3a
KI-RA-CO <kihara4173@icloud.com>
2020-02-21 13:44:56 +0900

    displayed new_item's_images on topPage
    トップページに、新規投稿商品を新しい順に表示させるようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @products = Product.includes(:images).where(status: 0).order("id DESC").limit(3)
+    
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,17 @@ ActiveRecord::Schema.define(version: 2020_02_18_054937) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "url", null: false
-    t.integer "products_id"
+    t.string "name", null: false
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -46,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_054937) do
     t.integer "send_price", default: 0, null: false
     t.integer "ship_day", default: 0, null: false
     t.integer "user_id", null: false
-    t.integer "small_category_id", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
