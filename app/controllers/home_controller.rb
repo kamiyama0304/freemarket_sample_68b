ebf90b013ba5600dc0c5756a205beb4bd1f8e2da
KI-RA-CO <kihara4173@icloud.com>
2020-02-26 14:23:17 +0900

    sacrifice for development

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ApplicationController
   def index
     @newProducts = Product.includes(:images).where(status: 0).order("RAND()")
     @brandProducts = Product.includes(:images).where(status: 0).order("id DESC")
-  end
     @parent = Category.where(ancestry: nil)
-    end
+  end
 end
 
